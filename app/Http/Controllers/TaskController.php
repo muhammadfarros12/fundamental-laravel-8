4ed174ae4d49f66d6e42c13f68d552bb0810847e
ammaar <muhammadfarroos12@gmail.com>
2023-01-30 22:46:23 +0700

    validate input request

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -32,6 +32,12 @@ class TaskController extends Controller
     }
 
     public function store(Request $request) {
+
+        $request->validate([
+            'task' => ['required'],
+            'user' => ['required']
+        ]);
+
         Task::create([
             'task' => $request->task,
             'user' => $request->user

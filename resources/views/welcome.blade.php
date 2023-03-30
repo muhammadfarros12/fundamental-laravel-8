4841818ed4c076da9f1b377cd777c439b5ff395c
ammaar <muhammadfarroos12@gmail.com>
2023-03-30 13:51:26 +0700

    delete some text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     
 <div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 380px;">
         <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
-            <span class="fs-5 fw-semibold">Welcome to Task Management Application</span>
+            <span class="fs-5 fw-semibold">Welcome to Task Management</span>
         </div>
  </div>
 
